fab4e5111233c23d24fecd23b713740fd3a80fae
Dan Fishgold <d.fishgold@gmail.com>
2018-06-29 21:32:23 +0300

    Add playback rate control

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,17 @@
-module Main exposing (..)
+port module Main exposing (..)
 
-import Html exposing (Html, program, div, input, p, h2, text, video, source)
+import Html exposing (Html, program, div, input, p, h2, text, video, button)
 import Html.Keyed
-import Html.Attributes exposing (dir, value, src, width, controls, autoplay, preload)
+import Html.Attributes exposing (dir, value, src, width, controls, autoplay, preload, disabled)
 import Html.Events exposing (onInput, onClick)
 import RemoteData exposing (WebData, RemoteData(..))
 import RemoteData.Http
 import Dict exposing (Dict)
 import Json.Decode as D
 import Fuzzy
+import Process
+import Time exposing (second)
+import Task
 
 
 main : Program Never Model Msg
@@ -25,6 +28,7 @@ type alias Model =
     { dictionary : WebData Dictionary
     , query : String
     , selectedWords : List String
+    , playbackRate : Float
     }
 
 
@@ -46,6 +50,7 @@ init =
     ( { dictionary = NotAsked
       , query = ""
       , selectedWords = []
+      , playbackRate = 1
       }
     , RemoteData.Http.get "http://localhost:8000/combined.json" SetDictionary dictionaryDecoder
     )
@@ -55,11 +60,28 @@ type Msg
     = SetDictionary (WebData Dictionary)
     | SetQuery String
     | ShowWords (List String)
+    | SetPlaybackRate Float
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
+
+
+port setPlaybackRate : Float -> Cmd msg
+
+
+{-|
+I need this because if I just use the port
+whenever I add video elements to the page
+the js code would run before they get rendered,
+so I need to add some delay.
+-}
+delayedSetPlaybackRate : Float -> Cmd Msg
+delayedSetPlaybackRate rate =
+    Process.sleep (0.03 * second)
+        |> Task.andThen (always <| Task.succeed <| SetPlaybackRate rate)
+        |> Task.perform identity
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -81,8 +103,11 @@ update msg model =
                 | selectedWords = ids
                 , query = ""
               }
-            , Cmd.none
+            , delayedSetPlaybackRate model.playbackRate
             )
+
+        SetPlaybackRate rate ->
+            ( { model | playbackRate = rate }, setPlaybackRate rate )
 
 
 sortIds : Dict String String -> List String -> List String
@@ -112,6 +137,10 @@ view model =
                                 [ text <| groupTitle groupBase ids ]
                         )
                     |> div []
+                , if List.length model.selectedWords > 0 then
+                    playbackRateControl model.playbackRate
+                  else
+                    text ""
                 , model.selectedWords
                     |> List.filterMap (\k -> Dict.get k words |> Maybe.map (\w -> ( k, w )))
                     |> List.map (\( id, word ) -> ( "video-" ++ word, video id word ))
@@ -148,3 +177,20 @@ video id word =
             ]
             []
         ]
+
+
+playbackRateControl : Float -> Html Msg
+playbackRateControl currentRate =
+    let
+        rateButton rate =
+            button
+                [ onClick <| SetPlaybackRate rate
+                , disabled <| rate == currentRate
+                ]
+                [ text <| "x" ++ toString rate ]
+    in
+        text "מהירות"
+            :: ([ 0.5, 0.75, 1 ]
+                    |> List.map rateButton
+               )
+            |> div []
