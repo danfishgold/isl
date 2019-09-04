3daa20e157a87fc2e19ea2f600bad403f6fbee16
Dan Fishgold <d.fishgold@gmail.com>
2019-09-04 09:00:05 -0700

    Add cache removal to the glitch config file

--- lws.config.js
+++ lws.config.js
@@ -1,3 +1,13 @@
-require('child_process').execSync('elm make src/Main.elm --output=elm.js', {
-  stdio: 'inherit',
-})
+const execSync = require('child_process').execSync
+
+try {
+  console.log(execSync('elm make src/Main.elm --output=elm.js').toString())
+} catch (err) {
+  if (err.message.includes('elm-stuff') || err.message.includes('/.elm')) {
+    execSync('rm -r elm-stuff')
+    execSync('rm -r .elm')
+    console.log(execSync('elm make src/Main.elm --output=elm.js').toString())
+  } else {
+    throw err
+  }
+}
